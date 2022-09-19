a74752462c96ac0c303777658f4ccb7c311354d3
Milana Vujanovic <89090425+Milana000@users.noreply.github.com>
2022-09-19 13:37:08 +0200

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,11 @@
+function openNav() {
+  document.getElementById("nav").style.width = "100%";
+  document.getElementById("nav").style.display = "block";
+  document.getElementById("closenav").style.display = "none";
+}
+
+function closeNav() {
+  document.getElementById("nav").style.width = "0%";
+  document.getElementById("nav").style.display = "none";
+  document.getElementById("closenav").style.display = "block";
+}
